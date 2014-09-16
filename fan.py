2258f02e8eda6e6f655d88dd2496544423289cd9
Ghanashyam <mail.ghanashyam.prabhu@intel.com>
2014-09-17 02:15:16 +0530

    Rudimentary code added and tested with Intel Galileo
    The code runs a periodic check on the CPU temperature and locates an index of the temperature
    Basic parameters include
    	CONST_MAX_TEMP - maximum temperature
    	CONST_MIN_TEMP - minimum temperature
    	CONST_STEP_TEMP - value of the step size when analyzing the current CPU temperature
    
    Once, the index is located, this is scaled to adjust the PWM duty cycle to control the voltage
    at the GPIO - the GPIO drives the base of a P2222 transistor in common emitter configuration
    which results in having a current gain at the collector on which the CPU Fan is connected
    
    math used here
    	Minimum PWM value to keep the motor running  + (index / number of steps between min & max)* Rest of the PWM duty cycle span count

--- fan.py
+++ fan.py
@@ -13,49 +13,114 @@ b)
 """
 
 import os
+import time
 
+#constants declaration 
+CONST_MIN_TEMP = 35
+CONST_MAX_TEMP = 80
+CONST_STEP_TEMP = 3
 
-
+# CPUFAN Class
+#CPUFAN is a class that monitors the temperature of the CPU and 
+#control the speed of the cooling fan to cool off the CPU
 class CPUFAN():
-    
-    
-    
+        
     def __init__(self, __DefaultName=None):
         
-        """
-        FIXME - add verbose/debug parsing code
-        """
+        #FIXME - add verbose/debug parsing code
         if __DefaultName == None:
-            self.name = "CPUFan"
+            self.name = 'CPUFan'
         else:
             self.name = __DefaultName
             
-    print "Initializing cpu fan object:". self.name
+        print "-INFO- :Creating CPUFAN object:", self.name,"\n"
+        print "-INFO- :INIT DONE\n"
+
+        #FIXME: check if fan on or off or available
+        #       If exits, initialize fan 
+
+        # Kick start with required vg to turn it ON
         
+        f=os.popen('echo -n "4" > /sys/class/pwm/pwmchip0/export')
+        f.close
+        f=os.popen('echo -n "1000000" > /sys/class/pwm/pwmchip0/pwm4/period')
+        f.close
+        f=os.popen('echo -n "900000" > /sys/class/pwm/pwmchip0/pwm4/duty_cycle')
+        f.close
+        f=os.popen('echo -n "1" > /sys/class/pwm/pwmchip0/pwm4/enable')
+        f.close
 
-    
-    """ 
-    CPUFAN is a class that monitors the temperature of the CPU and 
-    control the speed of the cooling fan to cool off the CPU
-    """
-
-    
-    """
-    function to read the CPU temperature
-    """
+        # calculate total gears
+        self.Gears = (CONST_MAX_TEMP - CONST_MIN_TEMP)/CONST_STEP_TEMP
+        
+    #Function to read the CPU temperature
     def ReadCPUTemperature(self):
         f=os.popen('cat /sys/class/thermal/thermal_zone0/temp')
-        CPUTemperature = f.read()
+        self.CPUTemperature = f.read()
         f.close()
-        
-        return CPUTemperature
+        return int(self.CPUTemperature)/1000
 
-    """pick range and mapping to speed"""
+    #Pick range and mapping to speed
+    def FindGearLevel(self):
+
+        #read temperature
+        self.tempCelsius = self.ReadCPUTemperature()
+
+        #find range 
+        GearLevel = 0
+        if (self.tempCelsius < CONST_MIN_TEMP):
+            self.GearLevel = 0
+        else:
+            self.GearLevel = 1
+            for i in range(CONST_MIN_TEMP, CONST_MAX_TEMP, CONST_STEP_TEMP):
+                if (self.tempCelsius in range (i,i+CONST_STEP_TEMP)):
+                    break
+                else:
+                    self.GearLevel+=1
     
-    """set run loop"""
+        return self.GearLevel
+
+    #fan control 
+    def FanSpeedControl(self):
+        self.GearLevel = self.FindGearLevel();
+        
+        if(self.GearLevel == 0):
+            # Turn Off fan 
+            print self.Gears
+            # not required to turn ON the fan
+            # turn OFF fan
+            self.cmdstring = ('echo -n \"0\" > /sys/class/pwm/pwmchip0/pwm4/duty_cycle')
+            return
+        
+        else:
+            
+            self.Offset = 400000 + self.GearLevel*600000/self.Gears
+            print self.GearLevel
+            print self.Gears
+            
+        self.cmdstring = ('echo -n \"' + str(self.Offset) + '\" > /sys/class/pwm/pwmchip0/pwm4/duty_cycle')
+        print self.cmdstring
+        print 'CPU temperature at ' + str(self.tempCelsius)
+
+        f=os.popen(self.cmdstring)
+        f.read()
+        f.close
+
+    def run(self):
+        while 1:
+            time.sleep(3)
+            self.FanSpeedControl();
 
 
-cpufanctrl = CPUFAN(self, "cpufanctrl")    
+
+
+#run program
+cpufanctrl = CPUFAN('cpufanctrl')
+cpufanctrl.run()
+
+
+
+  
     
 
     
